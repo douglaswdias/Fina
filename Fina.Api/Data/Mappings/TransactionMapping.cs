db38fcaf773a3c76e58bbe67823d6c55280a33d6
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-06-03 14:42:38 -0300

    feat: Create TransactionMapping

---
+++ Fina.Api/Data/Mappings/TransactionMapping.cs
@@ -0,0 +1,39 @@
+﻿using Fina.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fina.Api.Data.Mappings;
+
+public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.ToTable("Transaction");
+
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasColumnType("nvarchar")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.Type)
+            .IsRequired()
+            .HasColumnType("smallint");
+
+        builder.Property(x => x.Amount)
+            .IsRequired()
+            .HasColumnType("money");
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.Property(x => x.PaidOrReceivedAt)
+            .IsRequired(false);
+        
+        builder.Property(x => x.UserId)
+            .IsRequired()
+            .HasColumnType("nvarchar")
+            .HasMaxLength(160);
+    }
+}
