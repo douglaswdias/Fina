0853d2addb58cd05c5cfacaf36fff2b539437bd3
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-06-18 21:49:14 -0300

    add HttpClient

--- Fina.Web/Program.cs
+++ Fina.Web/Program.cs
@@ -1,6 +1,10 @@
+using Fina.Core;
+using Fina.Core.Handlers;
+using Fina.Core.Models;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Fina.Web;
+using Fina.Web.Handlers;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +13,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddHttpClient(
+        WebConfiguration.HttpClientName, options =>
+        {
+            options.BaseAddress = new Uri(Configuration.BackendUrl);
+        });
+
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
 await builder.Build().RunAsync();
